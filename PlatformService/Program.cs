69bbd9e5fc48861e263775fe832da76455618fc3
Ozodbek4252 <ozodbekmcfly@gmail.com>
2023-05-12 10:22:50 +0500

    Configure Repo services in Program.cs

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -4,8 +4,9 @@ using PlatformService.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(opt => 
-opt.UseInMemoryDatabase("InMem"));
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseInMemoryDatabase("InMem"));
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
