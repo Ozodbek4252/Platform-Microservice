9f041a9ca95c435a8244c3f25ed18f4bbb062481
Ozodbek4252 <ozodbekmcfly@gmail.com>
2023-05-12 10:46:16 +0500

    Seed added

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,30 @@
+using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public class PrepDb
+{
+    public static void Seed(IApplicationBuilder applicationBuilder)
+    {
+        using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+        {
+            var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding Data...");
+                context.Platforms.AddRange(
+                    new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We aleady have data");
+            }
+        }
+    }
+}
